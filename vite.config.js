a3152c809fffc543e5ee19e78a23b1fb1f962381
Xavier <xfavia@gmail.com>
2025-11-03 15:10:49 -0600

    dot

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: process.env.VITE_BASE_PATH || "/",
+	base: process.env.VITE_BASE_PATH || "./",
 	build: {
 		outDir: "docs",
 	},
